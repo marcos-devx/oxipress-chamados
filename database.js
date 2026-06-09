d30efafa38f768104d701b1037dbf60164b2e4d2
Marcos <marcosvinigpereira@gmail.com>
2026-06-09 09:52:21 -0300

    feat: use DB_PATH env var for persistent SQLite path

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const Database = require('better-sqlite3');
 const bcrypt = require('bcryptjs');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, 'chamados.db'));
+const db = new Database(process.env.DB_PATH || path.join(__dirname, 'chamados.db'));
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
 
